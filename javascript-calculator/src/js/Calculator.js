b7147a9be8e7c583f8c52437536924d592db2bbe
sosoYim <sohyung.yim@gmail.com>
2022-06-19 11:34:20 +0900

    fix: Adapte logic like window calculator
    - Calculate total with every thrid operator
    - Reinitialize with result
    - If entering number from result, it reinitialize with entered
      number

--- javascript-calculator/src/js/Calculator.js
+++ javascript-calculator/src/js/Calculator.js
@@ -5,14 +5,62 @@ export default class Calculator {
   constructor(number = 0) {
     this.#numbers = [number];
     this.#operators = [];
-    this.isFinish = false;
     this.total = number;
-    this.canAddNextNumber = false;
     this.formula = number + '';
+    this.completeNumber = true;
   }
 
-  setTotal(total) {
-    this.total = total;
+  // computed ===============================
+  // vue 에서 상태 변화에 따라 캐싱된 계산값들을 기억할 수 있는 computed에 해당
+  shouldGetResult() {
+    return this.#numbers.length >= 2 || this.#operators[this.#operators.length - 1] === '=';
+  }
+
+  shouldAddNumber() {
+    return !this.#numbers.length || this.#numbers.length === this.#operators.length;
+  }
+
+  computedFormula() {
+    const formula = this.#numbers.reduce((acc, cur, idx) => {
+      return acc + cur + (this.#operators[idx] ? this.#operators[idx] : '');
+    }, '');
+
+    this.formula = formula;
+  }
+
+  calculateTotal() {
+    this.total =
+      this.#numbers.length === 1
+        ? this.#numbers[this.#numbers.length - 1]
+        : this.#numbers.reduce(
+            (acc, cur, idx) =>
+              idx === 0 ? acc : Calculator.calculateNumbers(this.#operators[idx - 1], acc, cur),
+            this.#numbers[0]
+          );
+
+    return this.total;
+  }
+
+  // method ===============================
+  updateNumber(addedNumber, maxLength = 3) {
+    if (this.completeNumber) {
+      this.resetNumber(addedNumber);
+      return;
+    }
+
+    const actualNum = this.#numbers[this.#numbers.length - 1];
+    const isValidLength = actualNum.toString().length < maxLength;
+
+    if (isValidLength)
+      this.#numbers[this.#numbers.length - 1] = parseInt(actualNum.toString() + addedNumber);
+
+    this.completeNumber = false;
+  }
+
+  resetNumber(addedNumber = 0) {
+    this.#numbers = [addedNumber];
+    this.completeNumber = false;
+    this.total = 0;
   }
 
   setOperator(operator) {
@@ -21,58 +69,19 @@ export default class Calculator {
     } else {
       this.#operators[this.#operators.length - 1] = operator;
     }
-    this.canAddNextNumber = true;
-  }
-
-  getLastNum() {
-    return this.#numbers[this.#numbers.length - 1];
-  }
-
-  // type check
-  setLastNum(num) {
-    this.#numbers[this.#numbers.length - 1] = num;
-    this.canAddNextNumber = false;
   }
 
   addNextNum(num = 0) {
     this.#numbers[this.#numbers.length] = num;
-    this.canAddNextNumber = false;
+    this.completeNumber = false;
   }
 
-  computedFormula() {
-    const formula = this.#numbers.reduce((acc, cur, idx) => {
-      return idx === 0 ? acc + cur : acc + this.#operators[idx - 1] + cur;
-    }, '');
-
-    this.formula = this.canAddNextNumber
-      ? formula + this.#operators[this.#operators.length - 1]
-      : formula;
-  }
-
-  isNumberTurn() {
-    return this.#numbers.length === this.#operators.length;
-  }
-
-  getLastCalculation() {
-    return [this.#numbers[this.#numbers.length - 1], this.#operators[this.#operators.length - 1]];
-  }
-
-  calculateTotal() {
-    this.total = this.#numbers.reduce(
-      (acc, cur, idx) =>
-        idx === 0 ? acc : Calculator.calculateNumbers(this.#operators[idx - 1], acc, cur),
-      this.#numbers[0]
-    );
-    this.isFinish = true;
-    return this.total;
-  }
-
-  initialCalculator(number = 0) {
+  initCalculator(number = 0) {
     this.#numbers = [number];
-    this.total = 0;
+    this.total = number;
     this.#operators = [];
-    this.canAddNextNumber = false;
-    this.isFinish = false;
+    this.formula = number + '';
+    this.completeNumber = true;
   }
 
   static calculateNumbers(operator, num1, num2) {
