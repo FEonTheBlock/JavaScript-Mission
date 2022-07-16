3ea3137e7aa603f87bd74e51b910e167acf1dda7
sonwonjae <sonwj091552@gmail.com>
2022-07-16 17:08:20 +0900

    feat: jsx babel 설정 및 컴포넌트 수정

--- javascript-vendingmachine-precourse/src/layouts/VendingMachine/VendingMachine.tsx
+++ javascript-vendingmachine-precourse/src/layouts/VendingMachine/VendingMachine.tsx
@@ -5,25 +5,14 @@ import { Navigation } from '../../components';
 interface VendingMachineProps {}
 
 function VendingMachine({ children }: PropsWithChildren<VendingMachineProps>) {
-  // return (
-  //   <div>
-  //     <h1>자판기</h1>
-  //     <main>
-  //       <Navigation />
-  //       {children || []}
-  //     </main>
-  //   </div>
-  // );
-  return createElement(
-    'div',
-    null,
-    createElement('h1', null, '자판기'),
-    createElement(
-      'main',
-      null,
-      createElement(Navigation, null),
-      ...(children || [])
-    )
+  return (
+    <div>
+      <h1>자판기</h1>
+      <main>
+        <Navigation />
+        {children}
+      </main>
+    </div>
   );
 }
 
