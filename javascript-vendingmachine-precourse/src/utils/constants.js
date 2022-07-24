af044f5c33d26c80712e24b798994f9c8f4d93fe
jiownkim0131 <qhsh866@gmail.com>
2022-07-24 16:33:39 +0900

    feat: 에러처리를 위한 상수 추가

---
+++ javascript-vendingmachine-precourse/src/utils/constants.js
@@ -0,0 +1,8 @@
+export const ERRORS = {
+  PROD_ADD: {
+    EMPTY_PROD_NAME: '상품명을 입력해주세요.',
+    WRONG_PRICE_UNIT: '상품 가격은 10의 배수로 입력해주세요.',
+    WRONG_QUANTITY: '상품 수량은 0보다 큰 값으로 입력해주세요.',
+    DUPLICATE_PROD_NAME: '이미 등록된 상품입니다.',
+  },
+};
