bae7795ab46b6e324117af2be17511d64c3c8e61
jiownkim0131 <qhsh866@gmail.com>
2022-07-24 19:47:44 +0900

    feat: 상품 구매 유효성 검사를 위한 상수 추가

--- javascript-vendingmachine-precourse/src/utils/constants.js
+++ javascript-vendingmachine-precourse/src/utils/constants.js
@@ -9,4 +9,9 @@ export const ERRORS = {
     EMPTY_CHANGE_AMOUNT: '충전 금액은 0원보다 큰 값을 입력해주세요.',
     WRONG_CHANGE_UNIT: '충전 금액은 10의 배수로 입력해주세요.',
   },
+  PURCHASE: {
+    EMPTY_PRODUCT_LIST: '등록된 상품이 없습니다. 상품을 등록해주세요.',
+    EMPTY_CHANGE: '잔돈이 부족해 상품을 구매할 수 없습니다. 잔돈을 충전해주세요.',
+    LACK_INSERTED: '현재 투입된 금액으로 해당 상품을 구매할 수 없습니다.',
+  },
 };
