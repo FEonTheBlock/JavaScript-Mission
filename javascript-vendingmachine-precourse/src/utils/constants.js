f7764ccd2a384dd458656ccb02edfce7ee973e11
jiownkim0131 <qhsh866@gmail.com>
2022-07-24 18:25:56 +0900

    feat: 잔돈 교환 유효성 검사를 위한 상수 추가

--- javascript-vendingmachine-precourse/src/utils/constants.js
+++ javascript-vendingmachine-precourse/src/utils/constants.js
@@ -5,4 +5,8 @@ export const ERRORS = {
     WRONG_QUANTITY: '상품 수량은 0보다 큰 값으로 입력해주세요.',
     DUPLICATE_PROD_NAME: '이미 등록된 상품입니다.',
   },
+  CHANGE: {
+    EMPTY_CHANGE_AMOUNT: '충전 금액은 0원보다 큰 값을 입력해주세요.',
+    WRONG_CHANGE_UNIT: '충전 금액은 10의 배수로 입력해주세요.',
+  },
 };
