6083c76ae17804f8c045e178475e78661c1dba15
jiownkim0131 <qhsh866@gmail.com>
2022-07-24 18:28:03 +0900

    feat: 잔돈 교환 view 구현

---
+++ javascript-vendingmachine-precourse/src/ChangeManage/ChangeManageView.js
@@ -0,0 +1,88 @@
+import { $ } from '../utils/dom.js';
+import Dialog from '../utils/dialog.js';
+import { validateChange } from '../utils/validation.js';
+
+export default class ChangeManageView {
+  $vendingMachineChargeForm;
+  template = `
+    <section id="vending-machine-manage-section">
+      <form id="vending-machine-charge-form">
+        <fieldset>
+          <legend>잔돈 충전</legend>
+          <input type="number" id="vending-machine-charge-input" placeholder="충전할 금액"/>
+          <button id="vending-machine-charge-button">충전하기</button>
+        </fieldset>
+        <span id="vending-machine-charge-amount"></span>
+      </form>
+      <section id="current-charge-section">
+        <h2>현재 잔돈</h2>
+        <table id="current-charge-table"></table>
+      </section>
+    </section>
+  `;
+
+  // 초기 렌더링  (잔돈 충전 메뉴 + 현재 잔돈)
+  render(changeInfo) {
+    $('#current-menu').innerHTML = this.template;
+    this.updateChangeInfo(changeInfo);
+    this.$vendingMachineChargeForm = $('#vending-machine-charge-form');
+    this.$vendingMachineChargeForm.addEventListener('submit', e => this.onChangeInput(e));
+  }
+
+  updateChangeInfo(changeInfo) {
+    const { total, coins } = changeInfo;
+    $('#vending-machine-charge-amount').textContent = `현재 총액: ${total}원`;
+    $('#current-charge-table').innerHTML = '';
+    $('#current-charge-table').innerHTML = this.createChangeInfo(coins);
+  }
+
+  createChangeInfo(coins) {
+    const { coin500, coin100, coin50, coin10 } = coins;
+
+    return `
+      <thead>
+        <tr>
+          <th>동전</th>
+          <th>개수</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>500원</td>
+          <td>${coin500}</td>
+        </tr>
+        <tr>
+          <td>100원</td>
+          <td>${coin100}</td>
+        </tr>
+        <tr>
+          <td>50원</td>
+          <td>${coin50}</td>
+        </tr>
+        <tr>
+          <td>10원</td>
+          <td>${coin10}</td>
+        </tr>
+      </tbody>
+    `;
+  }
+
+  onChangeInput(e) {
+    e.preventDefault();
+    const [_, changeInput] = e.target;
+
+    try {
+      validateChange(+changeInput.value);
+      Dialog.show('잔돈이 성공적으로 충전되었습니다!');
+    } catch (error) {
+      Dialog.error(error);
+      return;
+    }
+
+    // 잔돈 충전 이벤트 발생
+    const newEvent = new CustomEvent('submitChange', {
+      detail: +changeInput.value,
+    });
+    this.$vendingMachineChargeForm.dispatchEvent(newEvent);
+  }
+}
